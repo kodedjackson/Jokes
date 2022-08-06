6560ebcee91a66a92f7c78cde2b3934f71da6f8c
Jackson Jackson <kodedjackson@gmail.com>
2022-08-07 00:42:58 +0100

    finished improvement

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
+const spinner = document.querySelector('#js-spinner');
 
 
 const newQoute = document.querySelector("#generate_new_qoute");
 const endpoint = 'https://api.whatdoestrumpthink.com/api/v1/quotes/random';
-
-function newjoke(){
-    newQoute;
-    console.log("The button was clicked" + newQoute)
-}
+newQoute.addEventListener('click', getQuote )
 
 
 async function getQuote(){
+    spinner.classList.remove('hidden');
+    generate_new_qoute.disabled = false;
     try {
         const responseFromApi = await fetch (endpoint);
         if (!responseFromApi.ok){
@@ -19,8 +18,12 @@ async function getQuote(){
         displayQuote(json.message);
     } catch (errorStopScript) {
         console.log(errorStopScript);
-        alert("failed to get new Qoute")
+        alert("failed to get new Qoute");
     }
+    finally {
+        generate_new_qoute.disabled = false;
+        spinner.classList.add('hidden');
+      }
 }
 
 
@@ -28,8 +31,4 @@ function displayQuote(qoute) {
     const quoteText = document.querySelector('#displaythisqoute');
     quoteText.innerHTML = qoute;
   }
-
-  function shareBtn(){
-    navigator.share(displayQuote);
-
-  }
+  
